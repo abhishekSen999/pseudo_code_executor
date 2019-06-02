023c75dd123ddd5a8baba03ddfcd761a778c2f0b
Abhishek Sen <abhishek.sen999@gmail.com>
2019-06-02 19:41:36 +0530

    starting

---
+++ src/Pseudo_code_executor.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Pseudo_code_executor {
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/ReadFile.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class ReadFile {
+    ReadFile() throws IOException
+    {
+
+    }
+}
